d86964649499172e2d95444dcead35b0a06e4518
Ilona Kedracka <ilona.kedracka@gmail.com>
2018-12-14 10:58:43 +0100

    added PromotionsController

---
+++ src/main/java/pl/java/workshops/rest/PromotionsController.java
@@ -0,0 +1,26 @@
+package pl.java.workshops.rest;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.java.workshops.inventory.ItemDictionaryReader;
+import pl.java.workshops.inventory.PromotionDictionaryReader;
+import pl.java.workshops.item.ShoppingItem;
+import pl.java.workshops.promotions.Promotion;
+import pl.java.workshops.promotions.SimplePercentagePromotion;
+
+import java.util.List;
+
+
+@RestController
+public class PromotionsController {
+
+    @Autowired
+    PromotionDictionaryReader reader;
+
+    @RequestMapping("/promotions")
+    public List<Promotion> index() {
+        return reader.read();
+
+    }}
